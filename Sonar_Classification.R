166e415a20abdf0c09a07ad6240787205c6b88b9
mwombeki <mwombeki.fabian@me.com>
2017-04-20 21:40:52 -0400

    formlation added

--- Sonar_Classification.R
+++ Sonar_Classification.R
@@ -41,37 +41,64 @@ myControl <- trainControl(
 )
 
 #Linear model
-model <- train(Class ~., Sonar, method = "glm",  trControl = myControl)
+model <- train(Class ~., Sonar, 
+               method = "glm",  
+               trControl = myControl
+               )
 #Print model 
 model
 plot(model)
 
 #Using RandomFrorrest with a unique traincontrol
-model3 <- train(Class ~., Sonar, method = "ranger",  trControl = myControl)
+model3 <- train(
+        Class ~., Sonar, 
+        method = "ranger",  
+        trControl = myControl
+        )
 
 #Using RandomFrorrest after Defining a tuneGrid of mtry(variables/predictors to use)
 myControl2 <- data.frame(mtry = c(2,5,10,20,60))
-model3 <- train(Class ~., Sonar, method = "ranger",  tuneGrid = myControl2)
+model3 <- train(
+        Class ~., Sonar, 
+        method = "ranger",  
+        tuneGrid = myControl2
+        )
 model3
 plot(model3)
 
 #Using RandomFrorrest with a tunelenth/default
-model2 <- train(Class ~., Sonar, method = "ranger", tuneLength = 10 )
+model2 <- train(
+        Class ~., Sonar, 
+        method = "ranger", 
+        tuneLength = 10
+        )
 plot(model2)
 model2
 
 #using glmnet with trainControl
-model4 <- train(Class ~., Sonar, method = "glmnet", trControl = myControl)
+model4 <- train(
+        Class ~., Sonar, 
+        method = "glmnet", 
+        trControl = myControl
+        )
 model4
 plot(model4) # with default tuneGrid  values of 3- Alpha 3-lambda (ROC vs alpha for each lamda/regularazation)
 max(model4$results$ROC)
 model4$bestTune
 
 # use a custom tuneGrid of defined alpha and lamda tune group 
-tuneGridx = expand.grid(alpha = 0:1,
-                       lambda = seq(0.0001, 1, length = 20))
+tuneGridx = expand.grid(
+        alpha = 0:1,
+        lambda = seq(0.0001, 1, length = 20
+                     )
+        )
 
-model5 <- train(Class ~., Sonar, method = "glmnet", tuneGrid = tuneGridx)
+model5 <- train(
+        Class ~., Sonar, 
+        method = "glmnet", 
+        tuneGrid = tuneGridx
+        )
+
 print(model5)
 plot(model5) # alpha = 0 is pure ridge regression, and alpha = 1 is pure lasso regression
 max(model5$result$Accuracy)
